59a08c94cdcd6f216abd67a33bf786ee685f6646
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-04-15 15:57:23 +0100

    Fill in pom details

--- source/build.gradle.kts
+++ source/build.gradle.kts
@@ -22,6 +22,8 @@ android {
         }
     }
 
+    // Create a single variant for publishing called "release". Add separate jars for javadoc
+    // and sources.
     publishing {
         singleVariant("release") {
             withSourcesJar()
@@ -40,12 +42,40 @@ publishing {
             artifactId = "source-android"
             version = libs.versions.libraryVersion.get()
 
+            pom {
+                name.set("Source Android")
+                description.set("Guardian design system library for Android")
+                url.set("https://github.com/guardian/source-android")
+                packaging = "aar"
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("ab-gnm")
+                        name.set("Aditya Bhaskar")
+                        email.set("aditya.bhaskar@guardian.co.uk")
+                        url.set("https://github.com/ab-gnm")
+                        organization {
+                            name.set("Guardian News & Media")
+                            url.set("https://www.theguardian.com")
+                        }
+                    }
+                }
+            }
+
+            // Use the artifacts called "release" for publishing.
             afterEvaluate {
                 from(components["release"])
             }
         }
     }
+
     repositories {
+        // Adds a task for publishing locally to the build directory.
         maven {
             name = "gusource"
             url = uri("${project.buildDir}/gusource")
