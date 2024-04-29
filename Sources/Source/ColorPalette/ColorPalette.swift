cc87bb91e380dd142cbb6e8e2be3eaff6e6f6e9a
Aoife McLaughlin <aoife.mclaughlin@guardian.co.uk>
2024-04-29 12:06:42 +0100

    add colour palette

---
+++ Sources/Source/ColorPalette/ColorPalette.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+/**
+ Module that defines the brand colours from [Source documentation](https://design.theguardian.com/#colour-palette).
+ This currently only holds references to the colours and extensions to assist with defining colours elsewhere.
+ */
+public enum ColorPalette {
+    public static let brand300 = UIColor.colorFromBundle(named: "Brand300")!
+    public static let brand400 = UIColor.colorFromBundle(named: "Brand400")!
+    public static let brand500 = UIColor.colorFromBundle(named: "Brand500")!
+    public static let brand600 = UIColor.colorFromBundle(named: "Brand600")!
+    public static let brand800 = UIColor.colorFromBundle(named: "Brand800")!
+    public static let brandAlt200 = UIColor.colorFromBundle(named: "BrandAlt200")!
+    public static let brandAlt400 = UIColor.colorFromBundle(named: "BrandAlt400")!
+    public static let neutral0 = UIColor.colorFromBundle(named: "Neutral0")!
+    public static let neutral7 = UIColor.colorFromBundle(named: "Neutral7")!
+    public static let neutral10 = UIColor.colorFromBundle(named: "Neutral10")!
+    public static let neutral20 = UIColor.colorFromBundle(named: "Neutral20")!
+    public static let neutral36 = UIColor.colorFromBundle(named: "Neutral36")!
+    public static let neutral38 = UIColor.colorFromBundle(named: "Neutral38")!
+    public static let neutral46 = UIColor.colorFromBundle(named: "Neutral46")!
+    public static let neutral60 = UIColor.colorFromBundle(named: "Neutral60")!
+    public static let neutral86 = UIColor.colorFromBundle(named: "Neutral86")!
+    public static let neutral93 = UIColor.colorFromBundle(named: "Neutral93")!
+    public static let neutral97 = UIColor.colorFromBundle(named: "Neutral97")!
+    public static let neutral100 = UIColor.colorFromBundle(named: "Neutral100")!
+    public static let culture700 = UIColor.colorFromBundle(named: "Culture700")!
+    public static let culture800 = UIColor.colorFromBundle(named: "Culture800")!
+    public static let sport400 = UIColor.colorFromBundle(named: "Sport400")!
+    public static let sport500 = UIColor.colorFromBundle(named: "Sport500")!
+}
