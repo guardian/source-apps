fa8e1041897883bd8a82f80d1025c45005336742
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-05-25 10:07:18 +0100

    Copy over `BaseDeviceConfig` and `PaparazziRules.kt`

---
+++ android/source/src/test/kotlin/com/gu/source/utils/paparazzi/BaseDeviceConfig.kt
@@ -0,0 +1,24 @@
+package com.gu.source.utils.paparazzi
+
+import app.cash.paparazzi.DeviceConfig
+
+/**
+ * Used to define the device variants to use for screenshot tests.
+ */
+enum class BaseDeviceConfig(val deviceConfig: DeviceConfig) {
+    Phone(DeviceConfig.PIXEL_6),
+    Tablet(DeviceConfig.PIXEL_C),
+}
+
+/**
+ * Used to define the font scale variants to use for screenshot tests.
+ * Have disabled the small && large variants to reduce the number of screenshots.
+ * @property value The float value corresponding to the scale.
+ */
+enum class FontScale(val value: Float) {
+    //    Small(0.85f),
+    Normal(value = 1f),
+
+    //    Large(1.15f),
+    ExtraLarge(value = 1.3f),
+}
---
+++ android/source/src/test/kotlin/com/gu/source/utils/paparazzi/PaparazziRules.kt
@@ -0,0 +1,48 @@
+package com.gu.source.utils.paparazzi
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import com.android.ide.common.rendering.api.SessionParams
+import com.android.resources.NightMode
+
+/**
+ * Creates a paparazzi instance for testing sub screen level components. The rule ensures that
+ * snapshots are shrunk to only cover the component's drawable area.
+ * @param deviceConfig Optional device config. Use [BaseDeviceConfig] to provide different devices for parametric
+ * tests.
+ */
+fun createComponentPaparazziRule(
+    deviceConfig: DeviceConfig = BaseDeviceConfig.Phone.deviceConfig,
+) = Paparazzi(
+    deviceConfig = deviceConfig,
+    showSystemUi = false,
+    renderingMode = SessionParams.RenderingMode.SHRINK,
+)
+
+/**
+ * Creates a paparazzi instance for testing sub screen level components. The rule ensures that
+ * snapshots are shrunk to only cover the component's drawable area.
+ * This variant takes a night mode parameter to allow for day & night mode tests on a phone device.
+ * @param nightMode night mode to use for the test.
+ */
+fun createComponentPaparazziRule(
+    nightMode: NightMode,
+) = Paparazzi(
+    deviceConfig = BaseDeviceConfig.Phone.deviceConfig.copy(nightMode = nightMode),
+    showSystemUi = false,
+    renderingMode = SessionParams.RenderingMode.SHRINK,
+)
+
+/**
+ * Creates a paparazzi instance for testing screen level components. The produced snapshots are
+ * device screen sized.
+ * @param deviceConfig device config for the test. Use [BaseDeviceConfig] to provide different
+ * devices for parametric tests.
+ */
+fun createScreenPaparazziRule(
+    deviceConfig: DeviceConfig,
+) = Paparazzi(
+    deviceConfig = deviceConfig,
+    showSystemUi = false,
+    renderingMode = SessionParams.RenderingMode.NORMAL,
+)
