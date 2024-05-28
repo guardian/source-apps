0c05c2adaacdbeae1c2ba563ad4c89902567a4db
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-05-28 09:31:47 +0100

    Update ktlint versions

--- android/source/src/test/kotlin/com/gu/source/utils/paparazzi/PaparazziRules.kt
+++ android/source/src/test/kotlin/com/gu/source/utils/paparazzi/PaparazziRules.kt
@@ -25,9 +25,7 @@ fun createComponentPaparazziRule(
  * This variant takes a night mode parameter to allow for day & night mode tests on a phone device.
  * @param nightMode night mode to use for the test.
  */
-fun createComponentPaparazziRule(
-    nightMode: NightMode,
-) = Paparazzi(
+fun createComponentPaparazziRule(nightMode: NightMode) = Paparazzi(
     deviceConfig = BaseDeviceConfig.Phone.deviceConfig.copy(nightMode = nightMode),
     showSystemUi = false,
     renderingMode = SessionParams.RenderingMode.SHRINK,
@@ -39,9 +37,7 @@ fun createComponentPaparazziRule(
  * @param deviceConfig device config for the test. Use [BaseDeviceConfig] to provide different
  * devices for parametric tests.
  */
-fun createScreenPaparazziRule(
-    deviceConfig: DeviceConfig,
-) = Paparazzi(
+fun createScreenPaparazziRule(deviceConfig: DeviceConfig) = Paparazzi(
     deviceConfig = deviceConfig,
     showSystemUi = false,
     renderingMode = SessionParams.RenderingMode.NORMAL,
