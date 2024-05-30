4028c06e577beb8eff5da67f66c789a7d1f4f830
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-05-30 18:33:48 +0100

    Add button variants to sample screen

--- android/sample/src/main/kotlin/com/gu/source/MainActivity.kt
+++ android/sample/src/main/kotlin/com/gu/source/MainActivity.kt
@@ -3,15 +3,20 @@ package com.gu.source
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gu.source.components.buttons.SourceButton
+import com.gu.source.components.buttons.SourceIconButton
+import com.gu.source.icons.Check
 import com.gu.source.presets.palette.Brand400
 import com.gu.source.presets.typography.TextArticle17
+import com.gu.source.theme.ReaderRevenueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +34,91 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!\nWe're the Guardian, the world's leading liberal voice.",
-        modifier = modifier,
-        style = Source.Typography.TextArticle17,
-        color = Source.Palette.Brand400,
-    )
+    Column {
+        Text(
+            text = "Hello $name!\nWe're the Guardian, the world's leading liberal voice.",
+            modifier = modifier,
+            style = Source.Typography.TextArticle17,
+            color = Source.Palette.Brand400,
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row {
+            SourceIconButton(
+                icon = Source.Icons.Base.Check,
+                priority = SourceButton.Priority.PrimaryOnWhite,
+                contentDescription = null,
+                onClick = {},
+                size = SourceButton.Size.XSmall,
+            )
+            SourceButton(
+                text = "Hello",
+                priority = SourceButton.Priority.SecondaryOnWhite,
+                onClick = {},
+                size = SourceButton.Size.XSmall,
+            )
+            SourceIconButton(
+                icon = Source.Icons.Base.Check,
+                priority = SourceButton.Priority.TertiaryOnWhite,
+                contentDescription = null,
+                onClick = {},
+                size = SourceButton.Size.XSmall,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        ReaderRevenueTheme {
+            Row(modifier = it) {
+                SourceIconButton(
+                    icon = Source.Icons.Base.Check,
+                    priority = SourceButton.Priority.PrimaryOnWhite,
+                    contentDescription = null,
+                    onClick = {},
+                    size = SourceButton.Size.Small,
+                )
+                SourceButton(
+                    text = "Hello",
+                    priority = SourceButton.Priority.PrimaryOnWhite,
+                    onClick = {},
+                    size = SourceButton.Size.Small,
+                )
+                SourceIconButton(
+                    icon = Source.Icons.Base.Check,
+                    priority = SourceButton.Priority.TertiaryOnWhite,
+                    contentDescription = null,
+                    onClick = {},
+                    size = SourceButton.Size.Small,
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row {
+            SourceIconButton(
+                icon = Source.Icons.Base.Check,
+                priority = SourceButton.Priority.PrimaryOnWhite,
+                contentDescription = null,
+                onClick = {},
+                size = SourceButton.Size.Medium,
+            )
+            SourceButton(
+                text = "Hello",
+                priority = SourceButton.Priority.SecondaryOnWhite,
+                onClick = {},
+                size = SourceButton.Size.Medium,
+            )
+            SourceIconButton(
+                icon = Source.Icons.Base.Check,
+                priority = SourceButton.Priority.TertiaryOnWhite,
+                contentDescription = null,
+                onClick = {},
+                size = SourceButton.Size.Medium,
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
