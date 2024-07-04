76304cc8550677a5e4c940e9dc32ec5e87d93f96
Aoife McLaughlin <aoife.mclaughlin@guardian.co.uk>
2024-07-04 17:42:11 +0100

    wip - add sample app for colour catalogue

---
+++ SourceSample/SourceSample/SourceSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceSampleApp.swift
+//  SourceSample
+//
+//  Created by Aoife McLaughlin on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SourceSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
