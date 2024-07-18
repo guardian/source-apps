02c3e2ce871d650607ee643acea5ee9a5d65a7f4
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-07-18 11:59:21 +0100

    Implement `PagerProgressIndicator` using a `LazyRow` for drawing.

--- android/source/src/main/kotlin/com/gu/source/components/pager/PagerProgressIndicator.kt
+++ android/source/src/main/kotlin/com/gu/source/components/pager/PagerProgressIndicator.kt
@@ -3,64 +3,94 @@
 package com.gu.source.components.pager
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.gu.source.Source
+import com.gu.source.presets.palette.Neutral46
+import com.gu.source.presets.palette.Neutral86
+import com.gu.source.presets.typography.Titlepiece70
 import kotlinx.coroutines.delay
+
+private const val ScaleOffsetOne = 0.75F
+private const val ScaleOffsetTwo = 0.5F
+private val SelectedItemSize = 16.dp
 
 /**
  * A pager progress indicator that displays a set of items that represent the pages in a pager.
  *
  * @param pagerState The [PagerState] that this indicator should be bound to.
  * @param modifier The modifier to apply to the layout.
- * @param maxVisibleItems The maximum number of items to display at once.
- * @param horizontalArrangement The [Arrangement] to use for the items.
- * @param itemsVerticalAlignment The vertical alignment to use for the items.
- * @param itemContent Slot to draw individual indicator item. Takes two arguments: the index of the
- * item and the index of the selected item.
+ * @param maxVisibleItems The maximum number of items to display at once. Defaults to 5.
+ * @param itemSpacing Horizontal spacing between items. Defaults to `4.dp`.
+ * @param selectedItemSize The size of each item. Defaults to `16.dp`. The adjacent items will be
+ * scaled down by `0.75`, and remaining items by `0.5`.
+ * @param itemsVerticalAlignment The vertical alignment to use for the items. Defaults to
+ * [Alignment.CenterVertically].
  */
 @Composable
-fun BasePagerProgressIndicator(
+fun PagerProgressIndicator(
     pagerState: PagerState,
     modifier: Modifier = Modifier,
     maxVisibleItems: Int = 5,
-    horizontalArrangement: Arrangement.Horizontal = Arrangement.spacedBy(4.dp),
+    itemSpacing: Dp = 4.dp,
+    selectedItemSize: Dp = SelectedItemSize,
     itemsVerticalAlignment: Alignment.Vertical = Alignment.CenterVertically,
-    itemContent: @Composable (index: Int, selectedIndex: Int) -> Unit,
 ) {
-    Row(
-        modifier = modifier,
+    val listState = rememberLazyListState()
+
+    val widthInPx = with(LocalDensity.current) { selectedItemSize.toPx() }
+
+    LaunchedEffect(pagerState.currentPage) {
+        val viewportSize = listState.layoutInfo.viewportSize
+
+        listState.scrollToItem(
+            index = pagerState.currentPage,
+            scrollOffset = (widthInPx / 2 - viewportSize.width / 2).toInt(),
+        )
+    }
+
+    LazyRow(
+        state = listState,
+        modifier = modifier.width((selectedItemSize + itemSpacing) * maxVisibleItems),
+        horizontalArrangement = Arrangement.spacedBy(itemSpacing),
         verticalAlignment = itemsVerticalAlignment,
-        horizontalArrangement = horizontalArrangement,
+        userScrollEnabled = false,
+        contentPadding = PaddingValues(horizontal = itemSpacing),
     ) {
-        val startIndex = when {
-            pagerState.currentPage < maxVisibleItems / 2f -> 0
-            pagerState.pageCount - pagerState.currentPage < maxVisibleItems / 2f -> {
-                pagerState.pageCount - maxVisibleItems
-            }
-
-            else -> pagerState.currentPage - maxVisibleItems / 2
-        }
-            .coerceAtLeast(0)
-
-        val endIndex = (startIndex + maxVisibleItems).coerceAtMost(pagerState.pageCount)
-
-        for (index in startIndex until endIndex) {
-            itemContent(index, pagerState.currentPage)
+        items(pagerState.pageCount) { pageIndex ->
+            PagerProgressItem(
+                index = pageIndex,
+                selectedIndex = pagerState.currentPage,
+                selectedIndicatorSize = selectedItemSize,
+                selectedColour = Color.Red,
+                unSelectedColour = Color.Gray,
+                modifier = Modifier.graphicsLayer {
+                    val size = when (pageIndex) {
+                        pagerState.currentPage -> 1f
+                        pagerState.currentPage - 1, pagerState.currentPage + 1 -> ScaleOffsetOne
+                        else -> ScaleOffsetTwo
+                    }
+                    scaleX = size
+                    scaleY = size
+                },
+            )
         }
     }
 }
@@ -77,16 +107,26 @@ private fun Preview() {
         }
     }
 
-    Column {
-        HorizontalPager(state = pagerState) { Text(text = it.toString()) }
-        BasePagerProgressIndicator(pagerState) { index, selectedIndex ->
-            PagerProgressItem(
-                index = index,
-                selectedIndex = selectedIndex,
-                selectedIndicatorSize = 16.dp,
-                selectedColour = Color.Red,
-                unSelectedColour = Color.Gray,
-            )
+    Column(modifier = Modifier.padding(16.dp)) {
+        HorizontalPager(state = pagerState) {
+            Box(
+                modifier = Modifier
+                    .size(400.dp)
+                    .background(Source.Palette.Neutral86, RoundedCornerShape(8.dp)),
+            ) {
+                Text(
+                    text = (it + 1).toString(),
+                    style = Source.Typography.Titlepiece70,
+                    modifier = Modifier.align(Alignment.Center),
+                    color = Source.Palette.Neutral46,
+                )
+            }
         }
+        PagerProgressIndicator(
+            pagerState = pagerState,
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .align(Alignment.CenterHorizontally),
+        )
     }
 }
