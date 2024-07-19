be4b551e6114a6ba9486d564dda5314555d2c2ca
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-07-19 12:45:56 +0100

    Create a new variant which takes button colours as argument. Set it up with default value to match designs

--- android/source/src/main/kotlin/com/gu/source/components/pager/PagerProgressBar.kt
+++ android/source/src/main/kotlin/com/gu/source/components/pager/PagerProgressBar.kt
@@ -8,14 +8,17 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.gu.source.Source
+import com.gu.source.components.buttons.ButtonColours
+import com.gu.source.components.buttons.SourceBaseIconButton
 import com.gu.source.components.buttons.SourceButton
-import com.gu.source.components.buttons.SourceIconButton
+import com.gu.source.components.buttons.toColours
 import com.gu.source.daynight.AppColour
 import com.gu.source.daynight.AppColourMode
 import com.gu.source.icons.ChevronLeft
@@ -23,6 +26,8 @@ import com.gu.source.icons.ChevronRight
 import com.gu.source.presets.palette.Neutral0
 import com.gu.source.presets.palette.Neutral10
 import com.gu.source.presets.palette.Neutral100
+import com.gu.source.presets.palette.Neutral7
+import com.gu.source.theme.LocalSourceTheme
 import com.gu.source.utils.PhoneBothModePreviews
 import com.gu.source.utils.TabletBothModePreviews
 import com.gu.source.utils.isTabletDevice
@@ -30,16 +35,29 @@ import kotlinx.coroutines.launch
 
 private enum class ProgressDirection {
     Previous,
-    Next
+    Next,
 }
 
 private val SelectedIndicatorColour = AppColour(
     light = Source.Palette.Neutral0,
     dark = Source.Palette.Neutral100,
 )
+
 private val UnSelectedIndicatorColour = AppColour(
     light = Source.Palette.Neutral0.copy(alpha = 0.2f),
     dark = Source.Palette.Neutral100.copy(alpha = 0.3f),
+)
+
+private val DefaultButtonColours = ButtonColours(
+    border = AppColour(
+        light = Source.Palette.Neutral7.copy(alpha = 0.2f),
+        dark = Source.Palette.Neutral100.copy(alpha = 0.3f),
+    ),
+    container = AppColour.Transparent,
+    content = AppColour(
+        light = Source.Palette.Neutral7,
+        dark = Source.Palette.Neutral100,
+    ),
 )
 
 /**
@@ -47,8 +65,8 @@ private val UnSelectedIndicatorColour = AppColour(
  * navigate between pages using next/prev buttons.
  *
  * @param pagerState The [PagerState] that this indicator should be bound to.
- * @param buttonPriority The priority for the next previous buttons - impacts the colour style.
  * @param modifier
+ * @param buttonColours The colours for the next/prev buttons.
  * @param selectedIndicatorColour The colour of the selected indicator item.
  * @param unSelectedIndicatorColour The colour of the unselected indicators items.
  */
@@ -56,8 +74,8 @@ private val UnSelectedIndicatorColour = AppColour(
 @Composable
 fun PagerProgressBar(
     pagerState: PagerState,
-    buttonPriority: SourceButton.Priority,
     modifier: Modifier = Modifier,
+    buttonColours: ButtonColours = DefaultButtonColours,
     selectedIndicatorColour: AppColour = SelectedIndicatorColour,
     unSelectedIndicatorColour: AppColour = UnSelectedIndicatorColour,
 ) {
@@ -78,7 +96,7 @@ fun PagerProgressBar(
         )
         if (isTabletDevice()) {
             ProgressButtons(
-                priority = buttonPriority,
+                buttonColours = buttonColours,
                 onClick = {
                     coroutineScope.launch {
                         val page = when (it) {
@@ -104,30 +122,69 @@ fun PagerProgressBar(
     }
 }
 
+/**
+ * A progress bar that shows the current page of a [PagerState] and, on tablets, allows the user to
+ * navigate between pages using next/prev buttons.
+ *
+ * @param pagerState The [PagerState] that this indicator should be bound to.
+ * @param buttonPriority The priority for the next previous buttons. Priority decides the button
+ * colours.
+ * @param modifier
+ * @param selectedIndicatorColour The colour of the selected indicator item.
+ * @param unSelectedIndicatorColour The colour of the unselected indicators items.
+ */
+
+@Suppress("CognitiveComplexMethod")
+@Composable
+fun PagerProgressBar(
+    pagerState: PagerState,
+    buttonPriority: SourceButton.Priority,
+    modifier: Modifier = Modifier,
+    selectedIndicatorColour: AppColour = SelectedIndicatorColour,
+    unSelectedIndicatorColour: AppColour = UnSelectedIndicatorColour,
+) {
+    PagerProgressBar(
+        pagerState = pagerState,
+        modifier = modifier,
+        buttonColours = buttonPriority.toColours(LocalSourceTheme.current),
+        selectedIndicatorColour = selectedIndicatorColour,
+        unSelectedIndicatorColour = unSelectedIndicatorColour,
+    )
+}
+
 @Composable
 private fun ProgressButtons(
-    priority: SourceButton.Priority,
+    buttonColours: ButtonColours,
     onClick: (progressDirection: ProgressDirection) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Row(modifier = modifier) {
-        SourceIconButton(
-            icon = Source.Icons.Base.ChevronLeft,
-            priority = priority,
+        SourceBaseIconButton(
+            buttonColours = buttonColours,
             size = SourceButton.Size.Small,
-            contentDescription = null,
             onClick = { onClick(ProgressDirection.Previous) },
-        )
-        SourceIconButton(
-            icon = Source.Icons.Base.ChevronRight,
-            priority = priority,
+        ) {
+            Icon(
+                imageVector = Source.Icons.Base.ChevronLeft,
+                contentDescription = null,
+                modifier = it,
+            )
+        }
+        SourceBaseIconButton(
+            buttonColours = buttonColours,
             size = SourceButton.Size.Small,
-            contentDescription = null,
             onClick = { onClick(ProgressDirection.Next) },
-        )
+        ) {
+            Icon(
+                imageVector = Source.Icons.Base.ChevronRight,
+                contentDescription = null,
+                modifier = it,
+            )
+        }
     }
 }
 
+@Suppress("MagicNumber")
 @PhoneBothModePreviews
 @TabletBothModePreviews
 @Composable
@@ -141,10 +198,7 @@ private fun Preview() {
             ),
         ) {
             HorizontalPager(state = pagerState) {}
-            PagerProgressBar(
-                pagerState = pagerState,
-                buttonPriority = SourceButton.Priority.TertiaryOnWhite,
-            )
+            PagerProgressBar(pagerState = pagerState)
         }
     }
 }
