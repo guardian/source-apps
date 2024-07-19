deb07008bff7044bb0bfa63e4a5e41e2abe8c441
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-07-19 16:49:53 +0100

    Create a `SampleImage` component that displays the placeholder in previews and fetched image on device.

---
+++ android/sample/src/main/kotlin/com/gu/source/SampleImage.kt
@@ -0,0 +1,43 @@
+package com.gu.source
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.painterResource
+import coil.compose.AsyncImage
+
+private val ImageUrl = "https://picsum.photos/695/544?random="
+
+/**
+ * Renders the appropriate Composable for [SampleImage]. In preview mode, a default placeholder
+ * image is displayed.
+ * @param modifier
+ * @param randomKey The random key to be appended to the image URL. If same key is passed, Coil may
+ * use the existing cached image again.
+ */
+@Composable
+internal fun SampleImage(
+    modifier: Modifier = Modifier,
+    randomKey: Int = 0,
+) {
+    val placeholder = if (LocalInspectionMode.current) {
+        painterResource(id = android.R.drawable.ic_menu_gallery)
+    } else {
+        null
+    }
+
+    val contentScale = if (LocalInspectionMode.current) {
+        ContentScale.Fit
+    } else {
+        ContentScale.Crop
+    }
+
+    AsyncImage(
+        modifier = modifier,
+        placeholder = placeholder,
+        contentDescription = null,
+        model = ImageUrl + randomKey,
+        contentScale = contentScale,
+    )
+}
