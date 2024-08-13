15d6ec0294dfbd67d1582cf302e79dd61509aaeb
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-08-13 13:00:01 +0100

    Add scaled font variant to multi previews

--- android/source/src/main/kotlin/com/gu/source/utils/PreviewAnnotations.kt
+++ android/source/src/main/kotlin/com/gu/source/utils/PreviewAnnotations.kt
@@ -1,5 +1,3 @@
-@file:Suppress("ktlint:compose:preview-annotation-naming")
-
 package com.gu.source.utils
 
 import android.content.res.Configuration
@@ -8,35 +6,50 @@ import androidx.compose.ui.tooling.preview.Preview
 
 /** Creates two previews on a Pixel 7 device for light and dark mode. */
 @Preview(
-    name = "Pixel 7 day mode",
+    name = "Day mode",
     device = Devices.PIXEL_7,
     uiMode = Configuration.UI_MODE_NIGHT_NO,
     backgroundColor = 0xFFFFFFFF,
     showBackground = true,
 )
 @Preview(
-    name = "Pixel 7 night mode",
+    name = "Night mode",
     device = Devices.PIXEL_7,
     uiMode = Configuration.UI_MODE_NIGHT_YES,
     backgroundColor = 0xFF000000,
     showBackground = true,
+)
+@Preview(
+    name = "Scaled font",
+    device = Devices.PIXEL_7,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    backgroundColor = 0xFFFFFFFF,
+    showBackground = true,
+    fontScale = 1.5f,
 )
 annotation class PreviewPhoneBothMode
 
 /** Creates two previews on a Pixel tablet device for light and dark mode. */
 @Preview(
-    name = "Pixel tablet day mode",
+    name = "Day mode",
     device = "spec:parent=pixel_tablet,orientation=portrait",
     uiMode = Configuration.UI_MODE_NIGHT_NO,
     backgroundColor = 0xFFFFFFFF,
     showBackground = true,
 )
 @Preview(
-    name = "Pixel tablet night mode",
+    name = "Night mode",
     device = "spec:parent=pixel_tablet,orientation=portrait",
     uiMode = Configuration.UI_MODE_NIGHT_YES,
     backgroundColor = 0xFF000000,
     showBackground = true,
 )
-annotation class TabletBothModePreviews
+@Preview(
+    name = "Scaled font",
+    device = "spec:parent=pixel_tablet,orientation=portrait",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    backgroundColor = 0xFFFFFFFF,
+    showBackground = true,
+    fontScale = 1.5f,
+)
 annotation class PreviewTabletBothMode
