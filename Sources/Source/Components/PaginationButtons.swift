7897a1e6acd0c02b8a8104d6e53dd0b9abdad1a8
Aoife McLaughlin <aoife.mclaughlin@guardian.co.uk>
2024-10-07 12:53:09 +0100

    make public

--- Sources/Source/Components/PaginationButtons.swift
+++ Sources/Source/Components/PaginationButtons.swift
@@ -1,13 +1,27 @@
 import SwiftUI
 
-struct PaginationButtons: View {
+public struct PaginationButtons: View {
     let iconColor: Color
     let borderColor: Color
     @Binding var selectedIndex: Int
     @Binding var canNavigateBack: Bool
     @Binding var canNavigateForward: Bool
 
-    var body: some View {
+    public init(
+        iconColor: Color,
+        borderColor: Color,
+        selectedIndex: Binding<Int>,
+        canNavigateBack: Binding<Bool>,
+        canNavigateForward: Binding<Bool>
+    ) {
+        self.iconColor = iconColor
+        self.borderColor = borderColor
+        self._selectedIndex = selectedIndex
+        self._canNavigateBack = canNavigateBack
+        self._canNavigateForward = canNavigateForward
+    }
+
+    public var body: some View {
         HStack {
             IconButton(
                 icon: Image(.chevronLeft),
