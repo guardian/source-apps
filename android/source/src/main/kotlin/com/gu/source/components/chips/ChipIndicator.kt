aee049658ee06374e689f779c24f730e5b94d5bb
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2024-12-07 12:27:53 +0000

    Add KDoc and preview for `ChipIndicator`

--- android/source/src/main/kotlin/com/gu/source/components/chips/ChipIndicator.kt
+++ android/source/src/main/kotlin/com/gu/source/components/chips/ChipIndicator.kt
@@ -1,34 +1,69 @@
 package com.gu.source.components.chips
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.gu.source.R
+import com.gu.source.Source
+import com.gu.source.components.chips.ChipIndicator.Icon
+import com.gu.source.components.chips.ChipIndicator.Image
+import com.gu.source.icons.Check
 
-// TODO: 07/12/2024 Add previews
-/** Represents an icon or image displayed before or after a [SourceChip]'s text. */
+/**
+ * Represents an icon or image displayed before or after a [SourceChip]'s text.
+ *
+ * [Icon]s are `18.dp` tall and the [Image]s are `24.dp` tall.
+ *
+ * The icon or image will be tinted with the [SourceChip.Style.contentColour]. To override the
+ * tinting, use [ChipIndicator.Icon.Component] or [ChipIndicator.Image.Component] and provide a
+ * custom composable function with the desired colour.
+ *
+ * For icons, prefer to use [ChipIndicator.Icon.Vector] with a [Source.Icons] vector where possible.
+ * Alternatively use [ChipIndicator.Icon.Painter] with a drawable resource ID.
+ *
+ * For images, prefer to use [ChipIndicator.Image.Painter] for static images. For images fetched
+ * from the network, use [ChipIndicator.Image.Component] with a custom composable function.
+ */
 sealed class ChipIndicator {
     /**
      * The content to display. The provided modifier _must_ be set on the content.
      * The modifier is used to apply the correct size to the icon/image.
      */
     abstract val content: @Composable RowScope.(Modifier) -> Unit
-    internal abstract val height: Dp
+
+    /** The height of the icon/image. */
+    abstract val height: Dp
 
     /**
-     * Represents an [Icon] displayed before/after the chip's text. Icon will be tinted with
-     * the [SourceChip.Style.contentColour]..
+     * Represents an [Icon] displayed before/after the chip's text. Icons are `18.dp` tall and will
+     * be tinted with the [SourceChip.Style.contentColour].
      */
     sealed class Icon : ChipIndicator() {
         override val height: Dp = 18.dp
 
+        /**
+         * Represents an [ImageVector] displayed before/after the chip's text. The icon will be
+         * `18.dp` tall and will be tinted with [SourceChip.Style.contentColour].
+         *
+         * @property imageVector The [ImageVector] to display.
+         * @property contentDescription Optional content description for the icon.
+         */
         data class Vector(
             val imageVector: ImageVector,
-            val contentDescription: String?,
+            val contentDescription: String? = null,
         ) : Icon() {
             override val content: @Composable RowScope.(Modifier) -> Unit = {
                 Icon(
@@ -39,9 +74,17 @@ sealed class ChipIndicator {
             }
         }
 
+        /**
+         * Represents a [androidx.compose.ui.graphics.painter.Painter] displayed before/after the
+         * chip's text. The icon will be `18.dp` tall and will be tinted with
+         * [SourceChip.Style.contentColour].
+         *
+         * @property painter The [androidx.compose.ui.graphics.painter.Painter] to display.
+         * @property contentDescription Optional content description for the icon.
+         */
         data class Painter(
             val painter: androidx.compose.ui.graphics.painter.Painter,
-            val contentDescription: String?,
+            val contentDescription: String? = null,
         ) : Icon() {
             override val content: @Composable RowScope.(Modifier) -> Unit = {
                 Icon(
@@ -52,18 +95,35 @@ sealed class ChipIndicator {
             }
         }
 
+        /**
+         * Represents a composable function that displays an icon before/after the chip's text.
+         * The icon will be `18.dp` tall and will be tinted with [SourceChip.Style.contentColour] if
+         * no color is set in the component.
+         *
+         * @property content The composable function to display the icon. The provided modifier
+         * _must_ be set on the content.
+         */
         data class Component(
             override val content: @Composable RowScope.(Modifier) -> Unit,
         ) : Icon()
     }
 
-    /** Represents an [Image] displayed before/after the chip's text. */
+    /**
+     * Represents an [Image] displayed before/after the chip's text. Images are `24.dp` tall.
+     */
     sealed class Image : ChipIndicator() {
         override val height: Dp = 24.dp
 
+        /**
+         * Represents an [ImageVector] displayed before/after the chip's text. The image will be
+         * `24.dp` tall and will be tinted with [SourceChip.Style.contentColour].
+         *
+         * @property imageVector The [ImageVector] to display.
+         * @property contentDescription Optional content description for the image.
+         */
         data class Vector(
             val imageVector: ImageVector,
-            val contentDescription: String?,
+            val contentDescription: String? = null,
         ) : Image() {
             override val content: @Composable RowScope.(Modifier) -> Unit = {
                 Image(
@@ -74,9 +134,17 @@ sealed class ChipIndicator {
             }
         }
 
+        /**
+         * Represents a [androidx.compose.ui.graphics.painter.Painter] displayed before/after the
+         * chip's text. The image will be `24.dp` tall and will be tinted with
+         * [SourceChip.Style.contentColour].
+         *
+         * @property painter The [androidx.compose.ui.graphics.painter.Painter] to display.
+         * @property contentDescription Optional content description for the image.
+         */
         data class Painter(
             val painter: androidx.compose.ui.graphics.painter.Painter,
-            val contentDescription: String?,
+            val contentDescription: String? = null,
         ) : Image() {
             override val content: @Composable RowScope.(Modifier) -> Unit = {
                 Image(
@@ -87,6 +155,13 @@ sealed class ChipIndicator {
             }
         }
 
+        /**
+         * Represents a composable function that displays an image before/after the chip's text.
+         * The image will be `24.dp` tall.
+         *
+         * @property content The composable function to display the image. The provided modifier
+         * _must_ be set on the content.
+         */
         data class Component(
             override val content: @Composable RowScope.(Modifier) -> Unit,
         ) : Image()
@@ -96,5 +171,42 @@ sealed class ChipIndicator {
     data object None : ChipIndicator() {
         override val content: @Composable RowScope.(Modifier) -> Unit = {}
         override val height: Dp = 0.dp
+    }
+}
+
+@SuppressLint("DiscouragedApi")
+@Preview
+@Composable
+private fun Preview() {
+    Row(
+        modifier = Modifier
+            .background(Color.Gray)
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        CompositionLocalProvider(LocalContentColor provides Color.White) {
+            listOf(
+                Icon.Vector(imageVector = Source.Icons.Base.Check),
+                Icon.Painter(painter = painterResource(id = R.drawable.ic_list)),
+                Icon.Component {
+                    Icon(
+                        imageVector = Source.Icons.Base.Check,
+                        contentDescription = "Check",
+                        modifier = Modifier.size(18.dp),
+                    )
+                },
+
+                Image.Vector(imageVector = Source.Icons.Base.Check),
+                Image.Painter(painter = painterResource(id = R.drawable.marina_hyde)),
+                Image.Component {
+                    Image(
+                        painter = painterResource(id = R.drawable.marina_hyde),
+                        contentDescription = "Marina Hyde",
+                        modifier = Modifier.size(24.dp),
+                    )
+                },
+            ).forEach { it.content(this, Modifier.height(it.height)) }
+        }
     }
 }
