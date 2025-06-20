21d6303363f847ed359ab3a1c83b4d200199766a
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2025-06-20 21:08:07 +0100

    Publish unsigned packages to given directory

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
     alias(libs.plugins.kotlinter) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.paparazzi) apply false
-    alias(libs.plugins.nexus.publish)
+//    alias(libs.plugins.nexus.publish)
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.dokka) apply false
     alias(libs.plugins.metalava) apply false
@@ -25,22 +25,22 @@ plugins {
 group = libs.versions.group.get()
 version = rootProject.file(libs.versions.versionFileName.get()).readText().trim()
 
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
-
-            // Sonatype token provides username and passwords as revokable secrets combined with a
-            // colon. We split them and provide it to the nexus plugin. See here for more:
-            // https://github.com/guardian/gha-scala-library-release-workflow/commit/23a148a03cf71bb2093a91f047d3c368adcdf45c
-            val token = System.getenv("AUTOMATED_MAVEN_RELEASE_SONATYPE_TOKEN") ?: ":"
-            val (uname, pwd) = token.split(":")
-            username = uname
-            password = pwd
-        }
-    }
-}
+//nexusPublishing {
+//    repositories {
+//        sonatype {
+//            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+//            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+//
+//            // Sonatype token provides username and passwords as revokable secrets combined with a
+//            // colon. We split them and provide it to the nexus plugin. See here for more:
+//            // https://github.com/guardian/gha-scala-library-release-workflow/commit/23a148a03cf71bb2093a91f047d3c368adcdf45c
+//            val token = System.getenv("AUTOMATED_MAVEN_RELEASE_SONATYPE_TOKEN") ?: ":"
+//            val (uname, pwd) = token.split(":")
+//            username = uname
+//            password = pwd
+//        }
+//    }
+//}
 
 allprojects {
     // Exclude generated files from linter
--- android/source/build.gradle.kts
+++ android/source/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `maven-publish`
-    signing
+//    signing
     alias(libs.plugins.guardian.library.android)
     alias(libs.plugins.guardian.compose.library)
     alias(libs.plugins.metalava)
@@ -102,11 +102,11 @@ publishing {
     }
 }
 
-signing {
-    useInMemoryPgpKeys(
-        System.getenv("AUTOMATED_MAVEN_RELEASE_PGP_SECRET") ?: "",
-        // We use a passwordless key so the an empty string is used as password here.
-        "",
-    )
-    sign(publishing.publications)
-}
+//signing {
+//    useInMemoryPgpKeys(
+//        System.getenv("AUTOMATED_MAVEN_RELEASE_PGP_SECRET") ?: "",
+//        // We use a passwordless key so the an empty string is used as password here.
+//        "",
+//    )
+//    sign(publishing.publications)
+//}
