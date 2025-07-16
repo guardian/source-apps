32c4dca7ebc5eed8197b009f5e7a3147f8468917
Benjamin Briggs <benjamin.briggs@guardian.co.uk>
2025-07-16 10:24:27 +0100

    Tidy up the chip palette

--- Sources/Source/Chips/ChipPalette.swift
+++ Sources/Source/Chips/ChipPalette.swift
@@ -2,7 +2,7 @@
 
 import SwiftUI
 
-/// Color palette for the chip views
+/// Colour palette for the chip views
 public struct ChipPalette {
     public var title: Color
     public var background: Color
@@ -24,70 +24,64 @@ public struct ChipPalette {
 
 /// Default palettes for the chip views
 public extension ChipPalette {
-    static var unselected: ChipPalette = {
-        ChipPalette(
-            title: Color(
-                .dynamicColor(
-                    light: ColorPalette.neutral10,
-                    dark: ColorPalette.neutral100
-                )
-            ),
-            background: Color(
-                .dynamicColor(
-                    light: ColorPalette.neutral93,
-                    dark: ColorPalette.neutral20
-                )
-            ),
-            badge: Color(hex: "0190F7"),
-            borderColor: Color(
-                .dynamicColor(
-                    light: ColorPalette.neutral93,
-                    dark: ColorPalette.neutral20
-                )
+    static let unselected = ChipPalette(
+        title: Color(
+            .dynamicColor(
+                light: ColorPalette.neutral10,
+                dark: ColorPalette.neutral100
+            )
+        ),
+        background: Color(
+            .dynamicColor(
+                light: ColorPalette.neutral93,
+                dark: ColorPalette.neutral20
+            )
+        ),
+        badge: Color(hex: "0190F7"),
+        borderColor: Color(
+            .dynamicColor(
+                light: ColorPalette.neutral93,
+                dark: ColorPalette.neutral20
             )
         )
-    }()
+    )
 
-    static var selected: ChipPalette = {
-        ChipPalette(
-            title: Color(
-                .dynamicColor(
-                    light: ColorPalette.neutral100,
-                    dark: ColorPalette.neutral10
-                )
-            ),
-            background: Color(
-                .dynamicColor(
-                    light: ColorPalette.neutral10,
-                    dark: ColorPalette.neutral93
-                )
-            ),
-            badge: Color(hex: "0190F7"),
-            borderColor: Color(
-                .dynamicColor(
-                    light: ColorPalette.neutral10,
-                    dark: ColorPalette.neutral93
-                )
+    static let selected = ChipPalette(
+        title: Color(
+            .dynamicColor(
+                light: ColorPalette.neutral100,
+                dark: ColorPalette.neutral10
+            )
+        ),
+        background: Color(
+            .dynamicColor(
+                light: ColorPalette.neutral10,
+                dark: ColorPalette.neutral93
+            )
+        ),
+        badge: Color(hex: "0190F7"),
+        borderColor: Color(
+            .dynamicColor(
+                light: ColorPalette.neutral10,
+                dark: ColorPalette.neutral93
             )
         )
-    }()
+    )
 
-    static var bordered: ChipPalette = {
-        ChipPalette(
-            title: Color(
-                .dynamicColor(
-                    light: ColorPalette.neutral10,
-                    dark: ColorPalette.neutral100
-                )
-            ),
-            background: Color(
-                .dynamicColor(
-                    light: ColorPalette.neutral100,
-                    dark: ColorPalette.neutral10
-                )
-            ),
-            badge: Color(hex: "0190F7"),
-            borderColor: Color(ColorPalette.neutral86)
-        )
-    }()
+    static let bordered = ChipPalette(
+        title: Color(
+            .dynamicColor(
+                light: ColorPalette.neutral10,
+                dark: ColorPalette.neutral100
+            )
+        ),
+        background: Color(
+            .dynamicColor(
+                light: ColorPalette.neutral100,
+                dark: ColorPalette.neutral10
+            )
+        ),
+        badge: Color(hex: "0190F7"),
+        borderColor: Color(ColorPalette.neutral86)
+    )
 }
