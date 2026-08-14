87e13060a6d68e6e1b3a9220f0f43c44734be17a
Aditya Bhaskar <79363218+adi-gnm@users.noreply.github.com>
2026-08-14 12:26:28 +0100

    Add `FilterScreenshots` and `Orientation`

--- android/source/src/test/kotlin/com/gu/source/utils/paparazzi/BaseDeviceConfig.kt
+++ android/source/src/test/kotlin/com/gu/source/utils/paparazzi/BaseDeviceConfig.kt
@@ -1,6 +1,7 @@
 package com.gu.source.utils.paparazzi
 
 import app.cash.paparazzi.DeviceConfig
+import com.android.resources.ScreenOrientation
 
 /**
  * Used to define the device variants to use for screenshot tests.
@@ -22,4 +23,13 @@ enum class FontScale(val value: Float) {
 
     //    Large(1.15f),
     ExtraLarge(value = 1.3f),
+}
+
+/**
+ * Used to define the orientation variants to use for screenshot tests.
+ * @property value The [com.android.resources.ScreenOrientation] value corresponding to the orientation.
+ */
+enum class Orientation(val value: ScreenOrientation) {
+    Portrait(ScreenOrientation.PORTRAIT),
+    Landscape(ScreenOrientation.LANDSCAPE),
 }
